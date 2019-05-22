e7b3ed70f3deb8db72cdcf14aa8bfcf83d338bf6
Andy Garcia <andreagarcia_sanchez@hotmail.com>
2019-05-21 19:43:39 -0500

    Se crea funcion para obtener la hora con parametro de UTC

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+window.calculateTimezone = {
+    updateHour: (offset) => {
+        let date = new Date(); // Generate time
+        let utc = date.getTime() + (date.getTimezoneOffset() * 60000); // Generate UTC
+        const dateUTC = new Date(utc + (3600000 * offset)); // Generate a new date with UTC
+        let hour = dateUTC.getHours();
+        let minutes = dateUTC.getMinutes();
+        let seconds = dateUTC.getSeconds();
+
+        if(hour < 10) {
+            hour = "0" + hour
+        };
+        if (minutes < 10) {
+            minutes = "0" + minutes
+        }; // Add a "0" if minutes are one digit
+        if (seconds < 10) {
+            seconds = "0" + seconds
+        }; // Add a "0" if seconds are one digit
+        return [hour, minutes, seconds]; // Return an array with the values of date
+    },
+};
